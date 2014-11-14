d0bc60e132dddf96d79d6c58559703056fd2e400
Fernando <GuruFern@gmail.com>
2014-11-13 21:17:31 -0500

    Added Givens For Personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,9 @@
 //Fernando P. López
 //November 13, 2014
 //Assignment: Conditionals_Personal
+
+//Do I need more Hot Pockets?
+//Based on how many Hot Pockets there are in the freezer and how many days in the week are left, is there enough to last the rest of the week?
+
+var daysInWeek = 7;
+var averageEatenPerWeek = 3; //Average amount of boxes of Hot Pockets eaten per week
