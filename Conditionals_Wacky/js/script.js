13f34072ae20f65b8d468c1d5149a266cb669158
Fernando <GuruFern@gmail.com>
2014-11-13 22:53:30 -0500

    Added Comments to Wacky. All done.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,12 +4,12 @@
 
 //If there are clouds in the sky then display a 2 sentence saying, otherwise show a 2 sentence saying for no clouds in the sky
 
-var ifClouds = "Clouds form beautiful shapes in the sky. Do you see the purple elephan over there?";
-var ifNoClouds = "Clear, Blue and Beautiful skies. Freedom is real and I can see nothing but the horizon.";
-var clouds = prompt("Is there clouds in the sky? Yes or No?");
+var ifClouds = "Clouds form beautiful shapes in the sky. Do you see the purple elephan over there?"; //If there are clouds this will be printed
+var ifNoClouds = "Clear, Blue and Beautiful skies. Freedom is real and I can see nothing but the horizon."; //If there are no clouds this will be printed
+var clouds = prompt("Is there clouds in the sky? Yes or No?"); //Prompt the user if there are clouds in the sky
 
-if (clouds === "") {
-	var clouds = prompt("Oops! You forgot to describe the sky to me. Are there clouds in the sky? Yes or No?")
+if (clouds === "") { //Validate clouds prompt
+	var clouds = prompt("Oops! You forgot to describe the sky to me. Are there clouds in the sky? Yes or No?") //Propmpt User for answer again
 };
 
-(clouds === "Yes" ) ? console.log(ifClouds) : console.log(ifNoClouds);
+(clouds === "Yes" ) ? console.log(ifClouds) : console.log(ifNoClouds); //Ternary with conditional looking for the string yes; If yes show ifClouds, If no show ifNoClouds
