ef24367d53bfc0317600f23c19aa6353421d2b1a
Fernando <GuruFern@gmail.com>
2014-11-13 22:02:09 -0500

    Fixed strictly equal to

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -8,13 +8,13 @@ var iPhoneWidth = 640;
 var iPhoneHeight = 1136;
 var iPadWidth = 2048;
 var iPadHeight = 1536;
-var device = prompt("Are you using an iPad or and iPhone?")
+var device = prompt("Are you using an iPad or an iPhone?")
 var butttonsAcross = prompt("How many buttons do you want across the screen");
 var buttonsTopBottom = prompt("How many buttons do you want top to bottom?");
 
-if(device === iPhone){
+if(device === "iPhone"){
 	console.log("Your buttons will have to be " + iPhoneWidth/butttonsAcross + "px wide and " + iPhoneHeight/buttonsTopBottom + "px high.");
-}else if (device === iPad) {
+}else if (device === "iPad") {
 	console.log("Your buttons will have to be " + iPadWidth/butttonsAcross + "px wide and " + iPadHeight/buttonsTopBottom + "px high.");
 }else {
 	console.log("Opps! Something went wrong. Please try again.");
