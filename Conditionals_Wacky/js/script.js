53e50b0e890bb5fe97bfcd744574018721d48a3d
Fernando <GuruFern@gmail.com>
2014-11-13 22:48:01 -0500

    Finished Variables and Filled Ternary

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,4 +4,8 @@
 
 //If there are clouds in the sky then display a 2 sentence saying, otherwise show a 2 sentence saying for no clouds in the sky
 
-() ? x : y;
+var ifClouds = "Clouds form beautiful shapes in the sky. Do you see the purple elephan over there?";
+var ifNoClouds = "Clear, Blue and Beautiful skies. Freedom is real and I can see nothing but the horizon.";
+var clouds = prompt("Is there clouds in the sky? Yes or No?");
+
+(clouds === Yes ) ? console.log(ifClouds) : console.log(ifNoClouds);
