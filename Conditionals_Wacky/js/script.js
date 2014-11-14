49ea8ae7ad463534b86345a023651de0d844b282
Fernando <GuruFern@gmail.com>
2014-11-13 22:38:43 -0500

    Main Wacky Comment and Ternary Basic Structure

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,3 +1,7 @@
 //Fernando P. López
 //November 13, 2014
 //Assignment: Conditionals_Wacky
+
+//If there are clouds in the sky then display a 2 sentence saying, otherwise show a 2 sentence saying for no clouds in the sky
+
+() ? x : y;
