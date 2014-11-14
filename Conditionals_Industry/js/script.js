7191ec09f92f554d3eb9420f2dcc5fed2f940dc6
Fernando <GuruFern@gmail.com>
2014-11-13 21:37:50 -0500

    Added Conditional and If Console Log

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,8 +9,8 @@ var deviceHeight = 1136;
 var butttonsAcross = prompt("How many buttons do you want across the screen on the iPhone?");
 var buttonsTopBottom = prompt("How many buttons do you want top to bottom?");
 
-if(){
-
+if(butttonsAcross && buttonsTopBottom != 0){
+	console.log("Oops! You missed an input! Please enter a value.");
 }else{
-
+	console.log
 };
