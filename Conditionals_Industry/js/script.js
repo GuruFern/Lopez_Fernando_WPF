3c7b8b04db61afd166e2158da4cf71a3997a45d1
Fernando <GuruFern@gmail.com>
2014-11-13 21:33:17 -0500

    Added Given and Prompts; Changed Logic

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,5 +2,10 @@
 //November 13, 2014
 //Assignment: Conditionals_Industry
 
-//What width do buttons have to be in px to fit side by side on an iOS screen
+//What width and height do buttons have to be in px to fit on an iPhone
+
+var deviceWidth = 640;
+var deviceHeight = 1136;
+var butttonsAcross = prompt("How many buttons do you want across the screen on the iPhone?");
+var buttonsTopBottom = prompt("How many buttons do you want top to bottom?");
 
