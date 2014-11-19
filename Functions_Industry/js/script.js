5a2636e2aeef1dab83a6894265b54f10d2786f71
GuruFern <flopez@TUSHAR-PATELs-Mac-Pro.local>
2014-11-19 13:56:47 -0500

    Setup JS Header with Main Idea

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,33 +1,8 @@
 //Fernando P. López
-//November 13, 2014
-//Assignment: Conditionals_Industry
+//November 20, 2014
+//Assignment: Functions_Industry
 
-//What width and height do buttons have to be in px to fit on an iPhone or an iPad screen
-
-var iPhoneWidth = 640; //iPhone Width
-var iPhoneHeight = 1136;//iPhone Height
-var iPadWidth = 2048;//iPad Width
-var iPadHeight = 1536;//iPad Height
-var device = prompt("Are you using an iPad or an iPhone?") //Prompt user what device will we us to calculate
-var butttonsAcross = prompt("How many buttons do you want across the screen"); //Prompt user for how many buttons acrosss
-var buttonsTopBottom = prompt("How many buttons do you want top to bottom?"); //Prompt user for hom many buttons top to bottom
-
-if(device === "") { //Validate if device is empty
-	var device = prompt("Hey! You forgot to enter your device. iPhone or iPad?"); //Prompt again and assign to device variable
-}
-
-if(butttonsAcross === "") { //Validate if buttonAcross is empty
-	var butttonsAcross = prompt("Hey! You forgot to enter how many buttons do you want accross on the screen."); //Prompt again and assign to buttonAcross variable
-}
-
-if(buttonsTopBottom === "") { //Validate if buttonsTopBottom is empty
-	var buttonsTopBottom = prompt("Hey! You forgot to enter how many buttons from top to bottom you want."); //Prompt again and assign to buttonsTopBottom variable
-}
-
-if(device === "iPhone"){ //If device variable is iPhone then return True
-	console.log("Your buttons will have to be " + iPhoneWidth/butttonsAcross + "px wide and " + iPhoneHeight/buttonsTopBottom + "px high."); //Print calculation for iPHone
-}else if (device === "iPad") { //If device variable is iPad then return True
-	console.log("Your buttons will have to be " + iPadWidth/butttonsAcross + "px wide and " + iPadHeight/buttonsTopBottom + "px high."); //Print calculation for iPad
-}else { //Otherwise
-	console.log("Opps! Something went wrong. Please try again."); //Print if user entered something other than iPHone or iPad in device variable
-}
+//Convert HEX to RGB and vice versa
+//Ask the user to enter whether they are starting with a HEX or RGB value.
+//Ask for value
+//Call function required and print result to user.
--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,15 +1,9 @@
 //Fernando P. López
-//November 13, 2014
-//Assignment: Conditionals_Wacky
+//November 20, 2014
+//Assignment: Functions_Wacky
 
-//If there are clouds in the sky then display a 2 sentence saying, otherwise show a 2 sentence saying for no clouds in the sky
-
-var ifClouds = "Clouds form beautiful shapes in the sky. Do you see the purple elephan over there?"; //If there are clouds this will be printed
-var ifNoClouds = "Clear, Blue and Beautiful skies. Freedom is real and I can see nothing but the horizon."; //If there are no clouds this will be printed
-var clouds = prompt("Is there clouds in the sky? Yes or No?"); //Prompt the user if there are clouds in the sky
-
-if (clouds === "") { //Validate clouds prompt
-	var clouds = prompt("Oops! You forgot to describe the sky to me. Are there clouds in the sky? Yes or No?") //Propmpt User for answer again
-};
-
-(clouds === "Yes" ) ? console.log(ifClouds) : console.log(ifNoClouds); //Ternary with conditional looking for the string yes; If yes show ifClouds, If no show ifNoClouds
+//How many bricks does the user need to build a wall of specified width and height
+//Ask for Wall Width
+//Ask for Wall Height 
+//Ask for Brick Size
+//Call Function that will perform calculation
