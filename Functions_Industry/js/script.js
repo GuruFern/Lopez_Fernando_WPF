a9ba44049afc8343888eb3d9379bfc58caf555fe
Fernando <GuruFern@gmail.com>
2014-11-20 23:51:59 -0500

    Commented Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,10 +2,18 @@
 //November 20, 2014
 //Assignment: Functions_Industry
 
-//Convert HEX to RGB and vice versa
+//Convert HEX to RGB
 //Ask the user to enter whether they are starting with a HEX or RGB value.
 //Ask for value
 //Call function required and print result to user.
 
-var hexOrRgb = prompt("Do you want to convert from hex to rgb or rgb to hex?");
-var value = prompt ("Please enter your value.");
+var objectWidth = prompt ("How wide is the object on the web page in pixels?");
+var objectHeight = prompt ("How tall is the object on the web page in pixels?");
+
+function objectArea(objectWidth, objectHeight) {
+	var totalArea = objectWidth * objectHeight;
+	return totalArea;
+};
+
+var area = objectArea(objectWidth, objectHeight);
+console.log("The total area of the object is " + area + "px.")
