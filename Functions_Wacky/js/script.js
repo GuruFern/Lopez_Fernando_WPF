9d2b9c7d305803f3d8037eb03041662ccd07fd8e
Fernando <GuruFern@gmail.com>
2014-11-20 23:19:34 -0500

    Created Anonymous Function

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -12,3 +12,8 @@ var wallWidth = prompt("How wide do you want to make your wall?");
 var wallHeight = prompt("How tall do you want to make your wall");
 var brickWidth = prompt("What width does the brick you are building with have?");
 var brickHeight = prompt("What height does the brick you are building with have?");
+
+var brickWallCalculator = function(wallWidth, wallHeight, brickWidth, brickHeight) {
+	var numberBricks = (wallWidth * wallHeight) / (brickWidth * brickHeight);
+	return numberBricks;
+};
