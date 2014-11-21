183798b8cf2ba387cf93e62355f4e071949baa3c
Fernando <GuruFern@gmail.com>
2014-11-20 22:22:17 -0500

    Changed Output and Added Initial Message

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,7 +7,8 @@
 //Ask user for amount of daily excercise
 //Call function that has calculation
 
-var weight = prompt("What is your current weight?");
+prompt("We will now calculate how much water you need per day in ounces.")
+var weight = prompt("What is your current weight in pounds?");
 var excercise = prompt("How much excercise do you do a day in minutes?");
 
 function waterCalculation (weight, excercise) {
@@ -16,4 +17,4 @@ function waterCalculation (weight, excercise) {
 };
 
 var total = waterCalculation(weight, excercise);
-console.log(total);
+console.log("You should drink a total of " + total + "oz of water per day.");
