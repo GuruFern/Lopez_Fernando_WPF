072c3718e55516ced58a4c9e4d9c95fb20bb15e4
Fernando <GuruFern@gmail.com>
2014-11-20 22:29:45 -0500

    Added Prompts to Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,3 +6,6 @@
 //Ask the user to enter whether they are starting with a HEX or RGB value.
 //Ask for value
 //Call function required and print result to user.
+
+var hexOrRgb = prompt("Do you want to convert from hex to rgb or rgb to hex?");
+var value = prompt ("Please enter your value.");
