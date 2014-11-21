9ca24901114f92b27213d593c3565399610490a8
Fernando <GuruFern@gmail.com>
2014-11-20 23:31:37 -0500

    Added Var and If Else Statement to Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,21 +2,26 @@
 //November 20, 2014
 //Assignment: Functions_Wacky
 
-//How many bricks does the user need to build a wall of specified width and height
+//Does the user have enough brick to build a wall of specified width and height
 //Ask for Wall Width
 //Ask for Wall Height 
-//Ask for Brick Size
-//Call Function that will perform calculation
+//Number of Bricks
 
 var wallWidth = prompt("How wide do you want to make your wall in feet?");
 var wallHeight = prompt("How tall do you want to make your wall in feet?");
-var brickWidth = prompt("What width in feet does the brick you are building with have?");
+var brickAmount = prompt("What width in feet does the brick you are building with have?");
 var brickHeight = prompt("What height in feet does the brick you are building with have?");
+var brickAmount = prompt("How many bricks do you have?")
 
 var brickWallCalculator = function(wallWidth, wallHeight, brickWidth, brickHeight) {
-	var numberBricks = (wallWidth * wallHeight) / (brickWidth * brickHeight);
-	return numberBricks;
+	var numberBricksNeeded = (wallWidth * wallHeight) / (brickWidth * brickHeight);
+	return numberBricksNeeded;
 };
 
-var totalNumberBricks = brickWallCalculator(wallWidth, wallHeight, brickWidth, brickHeight);
-console.log("You will need " + totalNumberBricks + " bricks to build your wall.");
+var numberOfBricks = brickWallCalculator(wallWidth, wallHeight, brickWidth, brickHeight);
+
+if (numberOfBricks >= brickAmount) {
+	console.log("You are ready to go! Start building that wall.");
+}else{
+	console.log("You do not have enough bricks! Go get some more.");
+};
