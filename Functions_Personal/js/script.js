b251d3ee1515bd94d55dcee34b4d3b8d49bb0e49
Fernando <GuruFern@gmail.com>
2014-11-20 23:54:19 -0500

    Commented Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,29 +7,29 @@
 //Ask user for amount of daily excercise
 //Call function that has calculation
 
-var weight = prompt("What is your current weight in pounds?");
-var excercise = prompt("How much excercise do you do a day in minutes?");
-var waterAmount = prompt("How much water do you dring in ounces per day?")
+var weight = prompt("What is your current weight in pounds?"); //Prompt user for current weight
+var excercise = prompt("How much excercise do you do a day in minutes?"); //Prompt user for daily excercise
+var waterAmount = prompt("How much water do you dring in ounces per day?") //Prompt user for daily water consumption
 
 if(weight === "") { //Validate if device is empty
-	var weight = prompt("Awww, don't be shy. What is your current weight in pounds?"); //Prompt again and assign to amountLeft variable
+	var weight = prompt("Awww, don't be shy. What is your current weight in pounds?"); //Prompt again and assign to weight variable
 }
 
 if(excercise === "") { //Validate if buttonAcross is empty
-	var excercise = prompt("Don't be bashful. How often do you flex them muscles?"); //Prompt again and assign to daysLeft variable
+	var excercise = prompt("Don't be bashful. How often do you flex them muscles?"); //Prompt again and assign to excercise variable
 }
 
 if(waterAmount === "") { //Validate if buttonAcross is empty
-	var waterAmount = prompt("Drinking water is important. How much water do you dring a day?"); //Prompt again and assign to daysLeft variable
+	var waterAmount = prompt("Drinking water is important. How much water do you dring a day?"); //Prompt again and assign to waterAmount variable
 }
 
-function waterCalculation (weight, excercise) {
+function waterCalculation (weight, excercise) { //Function calculating water needed
 	var waterTotal = (weight * .67) + (excercise * .4);
 	return waterTotal;
 };
 
-var total = waterCalculation(weight, excercise);
+var total = waterCalculation(weight, excercise); //Calling Function
 
-result = (waterAmount >= total) ? "You need to drink " + total + "oz of water per day and you are doing that and maybe even more!" : "You need to drink more water!";
+result = (waterAmount >= total) ? "You need to drink " + total + "oz of water per day and you are doing that and maybe even more!" : "You need to drink more water!"; //Ternary determining if you need to consume more water or not
 
-console.log(result);
+console.log(result); //Printing to Console
