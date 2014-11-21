d6c1bb6cfb376f3fe0fb25beae2c91ab5a2ecc83
Fernando <GuruFern@gmail.com>
2014-11-20 22:15:45 -0500

    Rearranged, Added Call and Print to Console

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,11 +7,13 @@
 //Ask user for amount of daily excercise
 //Call function that has calculation
 
-function waterCalculation (w, e) {
-	w * .67;
-	w + (e * .4);
-};
-
 var weight = prompt("What is your current weight?");
 var excercise = prompt("How much excercise do you do a day in minutes?");
 
+function waterCalculation (weight, excercise) {
+	var waterTotal = (weight * .67) + (excercise * .4);
+	return waterTotal;
+};
+
+var total = waterCalculation(weight, excercise);
+console.log(total);
